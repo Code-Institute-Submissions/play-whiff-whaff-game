24c9f8f5b96e91698a799bfb11ffca783375cddc
KrisK1978 <k_kempa@yahoo.co.uk>
2020-05-21 21:34:48 +0000

    Changing background colors for body elements

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,13 +3,11 @@
  */  
 const cvs = document.getElementById('gameCanvas');
 const ctx = cvs.getContext('2d');
-
 /** 
  * Declares canvas width & height
  */
 cvs.width = 600;
 cvs.height = 300;
-
 /**
  * Declares game elements
  */
@@ -21,7 +19,6 @@ const ping = {
     color: '#fde74c',
     score: 0
 };
-
 const pong = {
     x: cvs.width - 20,
     y: cvs.height/2 - 100/2,
@@ -30,7 +27,6 @@ const pong = {
     color: '#7a00f3',
     score: 0
 };
-
 const ball = {
     x: cvs.width/2,
     y: cvs.height/2,
@@ -40,7 +36,6 @@ const ball = {
     velocityY: 5,
     color: '#ea4d06'
 };
-
 const gameNet = {
     x: cvs.width/2 - 1,
     y: 0,
@@ -48,7 +43,6 @@ const gameNet = {
     height: 10,
     color: '#ff489f'
 };
-
 /** 
  * Draws a play-field
  */
@@ -56,7 +50,6 @@ let drawRect = (x, y, w, h, color) => {
     ctx.fillStyle = color;
     ctx.fillRect(x, y, w, h);
 };
-
 /**
  * Draws a ball 
  */ 
@@ -67,7 +60,6 @@ let drawCircle = (x, y, r, color) => {
     ctx.closePath();
     ctx.fill();
 };
-
 /**
  * Draws the game score
  */
@@ -76,7 +68,6 @@ let drawText = (text, x, y, color) => {
     ctx.fillStyle = color;
     ctx.fillText(text, x, y);
 };
-
 /**
  * Draws Player 1 
  */ 
@@ -84,7 +75,6 @@ let drawPlayerOne = () => {
     ctx.fillStyle = ping.color;
     ctx.fillRect(ping.x, ping.y, ping.width, ping.height);
 };
-
 /**
  * Draws Player 2 (pong)
  */
@@ -92,7 +82,6 @@ let drawPlayerTwo = () => {
     ctx.fillStyle = pong.color;
     ctx.fillRect(pong.x, pong.y, pong.width, pong.height);
 };
-
 /**
  * Draws the game net 
  */
@@ -101,15 +90,14 @@ let drawGameNet = () => {
         drawRect(gameNet.x, gameNet.y + i, gameNet.width, gameNet.height, gameNet.color);
     };
 };
-
 /**
  * Draws game elements - play-field, players, ball, net, score
  */ 
-let  callIn = () => {
+let callIn = () => {
     drawRect(0, 0, cvs.width, cvs.height, '#38cabd');
     drawCircle(ball.x, ball.y, ball.radius, ball.color);
     drawText(ping.score, cvs.width/4, cvs.height/8, '#fdfffc');
-    drawText(pong.score, 3*cvs.width/4, cvs.height/8, '#583043');
+    drawText(pong.score, 3*cvs.width/4, cvs.height/8,'#641220');
     drawPlayerOne();
     drawPlayerTwo();
     drawGameNet();
@@ -119,7 +107,6 @@ let  callIn = () => {
  * Initiates user controls 
  */
 cvs.addEventListener('mousemove', controlMove);
-
 /**
  * Controls player moves 
  */ 
@@ -127,7 +114,6 @@ function controlMove(e) {
     let rect = cvs.getBoundingClientRect();
     ping.y = e.clientY - rect.top - ping.height/2;
 };
-
 /**
  * Detects collision walls  
  */
@@ -136,22 +122,18 @@ let detectionWalls = (b, p) => {
     b.bottom = b.y + b.radius;
     b.left = b.x - b.radius;
     b.right = b.x + b.radius;
-
     p.top = p.y;
     p.bottom = p.y + p.height;
     p.left = p.x;
     p.right = p.x + p.width;
-
     return b.right > p.left && b.bottom > p.top && b.left < p.right && 
     b.top < p.bottom;
 
 };
-
 /**
  * Updates player's position/score/ball's speed & resets the ball.
  */
 let update = () => {
-
     ball.x += ball.velocityX;
     ball.y += ball.velocityY;
 
@@ -167,9 +149,7 @@ let update = () => {
     if(detectionWalls(ball, player)){
         let collidePoint = ball.y - (player.y + player.height/2);
         collidePoint = collidePoint/(player.height/2);
-
         let angleRad = collidePoint * Math.PI/4;
-
         let direction = (ball.x < cvs.width/2) ? 1 : -1;
         ball.velocityX = direction * ball.speed * Math.cos(angleRad);
         ball.velocityY = ball.speed * Math.sin(angleRad);
@@ -179,11 +159,14 @@ let update = () => {
     if(ball.x - ball.radius < 0) {
         pong.score++;
         resetBall();
+          
     }else if(ball.x + ball.radius > cvs.width) {
         ping.score++;
         resetBall();
     };
 };
+
+
 
 /**
  * Resets the ball.
@@ -198,10 +181,26 @@ let resetBall = () => {
 /**
  * Initiates the game.
  */
-let gameBoard = () => {
+let playGame = () => {
     callIn();
     update();
 };
 
 const framePerSecond = 50;
-setInterval(gameBoard, 1000/framePerSecond);
+setInterval(playGame, 1000/framePerSecond);
+
+const startButton = document.getElementById('openGame');
+const closeButton = document.getElementById('closeGame');
+
+startButton.onclick = function() {
+    cvs.style.display = 'block';
+};
+
+closeButton.onclick = function() {
+    cvs.style.display = 'none';
+};
+
+
+
+
+
